fe38fcaa5e938269b2109436b8954085e0c0bf08
Adeoluwa Adeboye <adeneye97@yahoo.com>
2023-02-15 16:57:06 +0000

    Added function for connecting to mongodb using uri

--- index.ts
+++ index.ts
@@ -1,13 +1,13 @@
 import app from "./app"
 import dotenv from "dotenv";
-// import connectDB from "./utils/db";
+import connectDB from "./utils/db";
 
 dotenv.config();
 const port = process.env.PORT;
 
 app.listen(port, async () => {
     // Initialize MongoDB connection
-    // await connectDB();
+    await connectDB();
 	console.log(
 		`Listening on ${port}, App is running at http://localhost:${port}`
 	);
